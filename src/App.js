049f2d256a6e76e514a9a88f0aa1b7c1587f17f7
David Ryan <davidryan1998@hotmail.com>
2020-03-08 17:34:29 +0700

    Polyphonic sequencer added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,90 @@
 import React from 'react';
+import * as Tone from 'tone'
 import logo from './logo.svg';
 import './App.css';
 
+// Overall constants
+const maxGain = 0.5
+const startDelayS = 0.5
+const beatTimeS = 0.2
+const defaultSlideBeats = 0.05
+
+// Sequenced chords
+// [[timing], [frequencies]]
+// [[timeTotalS, timeSlideS], [baseFreqHz, [relFreq1, relFreq2, relFreq3...]]]
+const polyphonySequence = [
+  [[4   ], [100,     [2,  3,  4,  5,  6]]],
+  [[4   ], [100,     [3,  4,  5,  6,  8]]],
+  [[2   ], [150,     [2,  3,  4,  5,  7]]],
+  [[2   ], [150,     [3,  4,  5,  6,  8]]],
+  [[4, 2], [100*4/3, [2,  3,  4,  7, 10]]],
+  [[4   ], [100,     [2,  4,  5,  6, 16]]],
+]
+const numSeqs = polyphonySequence.length
+const numChannels = polyphonySequence[0][1][1].length
+
+const n = {}  // All Nodes
+n.oscs = new Array(numChannels)
+n.gains = new Array(numChannels)
+for (let i=0; i<numChannels; i++) {
+  n.oscs[i] = new Tone.Oscillator({type: 'sine', frequency: 440})
+  n.gains[i] = new Tone.Gain({gain: maxGain / numChannels})
+  n.oscs[i].connect(n.gains[i])
+  n.gains[i].toMaster()
+}
+
+const sequenceAndStartNotes = () => {
+  const timeStartS = Tone.now() + startDelayS
+  let timeThisStartS = timeStartS
+  for (let i=0; i<numSeqs; i++) {
+    const beatLengths = polyphonySequence[i][0]
+    const timeTotalS = beatTimeS * beatLengths[0]
+    const timeSlideS = beatTimeS * ((Number.isFinite(beatLengths[1])) ? beatLengths[1] : defaultSlideBeats)
+    const timeLevelS = timeTotalS - timeSlideS
+    for (let j=0; j<numChannels; j++) {
+      const freqParam = n.oscs[j].frequency
+      const freqsThis = polyphonySequence[i][1]
+      const freqsNext = (polyphonySequence[i+1]) ? polyphonySequence[i+1][1] : freqsThis
+      const freqThisHz = freqsThis[0] * freqsThis[1][j]
+      const freqNextHz = freqsNext[0] * freqsNext[1][j]
+      freqParam.setValueAtTime(freqThisHz, timeThisStartS)
+      freqParam.linearRampTo(freqNextHz, timeSlideS, timeThisStartS + timeLevelS)
+    }
+    timeThisStartS += timeLevelS + timeSlideS
+  }
+  n.oscs.forEach( osc => {
+    osc.start(timeStartS)
+    osc.stop(timeThisStartS)
+  })
+}
+
+const stopNotes = () => {
+  n.oscs.forEach(osc => {
+    osc.frequency.cancelScheduledValues()
+    osc.stop()
+  })
+}
+
 const doStart = e => {
-  console.log('Starting...')
+  console.log(`Starting...`)
+  sequenceAndStartNotes()
 }
 
 const doStop = e => {
-  console.log('Stopping...')
+  console.log(`Stopping...`)
+  stopNotes()
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Just Intonation (JI) Polyphony App by David Ryan</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type="button" value="Play" onClick={doStart} />
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="button" value="Stop" onClick={doStop} />
         </p>
-        <input type="button" value="Play nice music" onClick={doStart} />
-        <input type="button" value="Stop nice music" onClick={doStop} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
