0d0dd92266ec88fed813988e7e18f1a546561b68
David Ryan <davidryan1998@hotmail.com>
2020-03-08 15:43:25 +0700

    Added start and stop buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const doStart = e => {
+  console.log('Starting...')
+}
+
+const doStop = e => {
+  console.log('Stopping...')
+}
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +18,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="button" value="Play nice music" onClick={doStart} />
+        <input type="button" value="Stop nice music" onClick={doStop} />
         <a
           className="App-link"
           href="https://reactjs.org"
