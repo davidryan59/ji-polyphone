83f555c263f1ce7e797dcec900ddc99fac482fda
David Ryan <davidryan1998@hotmail.com>
2020-03-22 12:02:26 +0700

    Recording added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import * as Tone from 'tone'
+import Recorder from 'recorder-js'
 import './App.css';
 import { sequenceSetup, polyphonySequence } from './seq/sequence'
 
-// Overall constants
+
+// Default constants here
 const c = {}
 c.maxGain = 0.2
 c.startDelayS = 0.5
 c.beatTimeS = 0.17
 c.defaultSlideBeats = 0.01
-c.baseFreq = 1.0249348  // make all frequencies slightly irrational
+c.baseFreq = 261.63
 c.deltaTime = 0.00001
 c.deltaGain = 0.000001
 c.justBelow1 = 0.999
 // Override these with any constants from sequence file itself
 Object.assign(c, sequenceSetup)
+
+
+// Setup audio context and recording
+const aCtx = new (window.AudioContext || window.webkitAudioContext)();
+// Using both Web Audio API, and Tone.js, so set contexts to be the same
+Tone.context = aCtx
+const recorder = new Recorder(aCtx);
+const recordingDestination = aCtx.createMediaStreamDestination();
+recorder.init(recordingDestination.stream)
 
 
 const numSeqs = polyphonySequence.length
@@ -85,12 +96,19 @@ n.delayComponentL = delayComponentGenerator(delayTimesFn)
 n.delayComponentR = delayComponentGenerator(delayTimesFn)
 n.panL = new Tone.Panner(-1)
 n.panR = new Tone.Panner(1)
+n.output = new Tone.Gain()
 
 // Connect components
 n.oscGainPost.connect(n.delayComponentL.input)
 n.oscGainPost.connect(n.delayComponentR.input)
-n.delayComponentL.output.chain(n.panL, Tone.Master)
-n.delayComponentR.output.chain(n.panR, Tone.Master)
+n.delayComponentL.output.chain(n.panL, n.output)
+n.delayComponentR.output.chain(n.panR, n.output)
+
+
+// Connect synth to speakers and to recorder
+n.output.connect(Tone.Master)
+n.output.connect(recordingDestination)
+
 
 const cancelScheduledValues = () => {
   n.oscs.forEach(osc => {
@@ -133,15 +151,31 @@ const stopNotes = () => {
   })
 }
 
-const doStart = e => {
-  console.log(`Starting...`)
+
+// Setup recording
+let isRecording = false;
+let blobToDownload = null;
+
+const playButton = () => {
+  console.log('Play button pressed')
   sequenceAndStartNotes()
+  recorder.start().then(() => isRecording = true);
 }
 
-const doStop = e => {
-  console.log(`Stopping...`)
+const stopButton = () => {
+  console.log('Stop button pressed')
   stopNotes()
+  recorder.stop().then( ({blob}) => {
+    isRecording = false
+    blobToDownload = blob
+  })
 }
+
+const downloadButton = () => {
+  console.log('Download button pressed')
+  Recorder.download(blobToDownload, 'Recording-' + new Date().toISOString()); // downloads a .wav file
+}
+
 
 function App() {
   return (
@@ -149,13 +183,16 @@ function App() {
       <header className="App-header">
         <p>Just Intonation (JI) Polyphony App by David Ryan</p>
         <p>
-          <input type="button" value="Play" onClick={doStart} />
+          <input type="button" id="play" value="Play" onClick={playButton} />
           &nbsp;&nbsp;&nbsp;&nbsp;
-          <input type="button" value="Stop" onClick={doStop} />
+          <input type="button" id="stop" value="Stop" onClick={stopButton}  />
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="button" id="download" value="Download" onClick={downloadButton}  />
         </p>
       </header>
     </div>
   );
 }
+
 
 export default App;
